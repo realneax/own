13f1f4812abef747173014a15648f46a902f7dd4
realneax <32615324+realneax@users.noreply.github.com>
2018-03-26 13:46:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 const Discord = require("discord.js");
+
+
 const client = new Discord.Client({
     owner: '285887620813160450'
 });
@@ -36,17 +38,51 @@ client.on("message", async message => {
   const command = args.shift().toLowerCase();
 
 
+  if(command === "ping") {
+
+    const m = await message.channel.send("Ping?");
+    m.edit(`Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+
   if(command === "e") {
-  if (message.author.id !== '285887620813160450')
-  return;
-  const sayMessage = args.join(" ");
+    if (message.author.id !== '285887620813160450')
+    return;
+    const sayMessage = args.join(" ");
 
-  message.delete().catch(O_o=>{});
+    message.delete().catch(O_o=>{});
 
-  message.channel.send({embed:{
-    color: 0x000000,
-    description: sayMessage
-  }})
-}
+    message.channel.send({embed:{
+      color: 0x000000,
+      description: sayMessage
+    }})
+  }
+
+  if(message.content.startsWith(config.prefix+"8ball")) {
+    if(!args[2]) return message.reply("Please ask a full quesition");
+    let replies = ["Yes", "No", "I don't know", "Ask later again"];
+
+    let result = Math.floor((Math.random() * replies.length));
+    let question = args.slice(0).join(" ");
+
+    let ballembed = new Discord.RichEmbed()
+    .setAuthor(message.author.tag)
+    .setColor("#0FF469")
+    .addField("Question", question)
+    .addField("Answer", replies[result]);
+
+      message.channel.send(ballembed);
+ }
+
+   if(command === "a") {
+    if (message.author.id !== '285887620813160450')
+    return;
+    const sayMessage = args.join(" ");
+
+    message.delete().catch(O_o=>{});
+
+    message.channel.send(sayMessage)
+  }
+
+});
 
 client.login(process.env.BOT_TOKEN);
