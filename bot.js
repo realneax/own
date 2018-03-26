882a4412364de8c286d55186c1c6b34410604b69
realneax <32615324+realneax@users.noreply.github.com>
2018-03-26 13:38:40 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({
+    owner: '285887620813160450'
+});
+
+
+const config = require("./config.json");
+
+client.on("ready", () => {
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+
+client.on("message", async message => {
+
+  if(message.author.bot) return;
+
+  if(message.content.indexOf(config.prefix) !== 0) return;
+
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+
+  if(command === "e") {
+  if (message.author.id !== '285887620813160450')
+  return;
+  const sayMessage = args.join(" ");
+
+  message.delete().catch(O_o=>{});
+
+  message.channel.send({embed:{
+    color: 0x000000,
+    description: sayMessage
+  }})
+}
